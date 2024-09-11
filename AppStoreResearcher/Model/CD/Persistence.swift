813a4be0db3d4c682b2a117bece971395b24687b
fDynamo <fontaine.dynamo@gmail.com>
2024-09-11 10:25:35 -0700

    modeling changes

--- AppStoreResearcher/Model/CD/Persistence.swift
+++ AppStoreResearcher/Model/CD/Persistence.swift
@@ -14,28 +14,12 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         
-        // Create groups
-        var groupIdList: [UUID] = []
-        for _ in 0..<5 {
-            let newItem = PageGroup(context: viewContext)
-            newItem.id = UUID()
-            groupIdList.append(newItem.id!)
-            newItem.title = ["Journaling", "Fitness","Action games", "Anti smoking", "Focus"].randomElement()
-        }
-        
-        // Create items
-        for _ in 0..<15 {
-            let newItem = PageItem(context: viewContext)
-            newItem.id = UUID()
-            newItem.group_id = groupIdList.randomElement()
-            newItem.app_title = ["Focus Duck", "Pomo Focus","Puff Quit", "Smoke free", "Ad Blocker", "Temple Run", "Flappy Bird", "Golf Mini", "Screen Zen"].randomElement()
-        }
+        // TODO: Fill with dummy data
 
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            // TODO: Handle error
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
@@ -51,9 +35,8 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 
+                // TODO: Handle error
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
