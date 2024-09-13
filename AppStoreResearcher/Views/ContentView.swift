d19dbac313434dc985b1fc7ff49e860bcff68b75
fDynamo <fontaine.dynamo@gmail.com>
2024-09-13 09:55:01 -0700

    moved content view states to VM

--- AppStoreResearcher/Views/ContentView.swift
+++ AppStoreResearcher/Views/ContentView.swift
@@ -9,44 +9,23 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @State private var showDetail = false
-    @State private var selectedGroupId: PageGroup.ID? = nil
-    @State private var contentTitle: String = "ASR"
-    @State private var viewMode: ViewMode = .OVERVIEW
-
-    private var selectedGroup: PageGroup? {
-        if let selectedGroupId = selectedGroupId {
-            return groupItems.first(where: {e in e.id == selectedGroupId})
-        }
-        
-        return nil
-    }
+    @State private var vm = ViewModel()
     
-//    private var selectedGroupPageItems: [PageItem] {
-//        if let sg = selectedGroup {
-//            let arr = sg.page_items?.array as? [PageItem] ?? []
-//            return arr
-//        }
-//        
-//        return []
-//    }
-
+    @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(key: "group_name", ascending: true)],
         animation: .default) private var groupItems: FetchedResults<PageGroup>
 
     var body: some View {
         NavigationSplitView {
-            List(groupItems, selection: $selectedGroupId){
+            List(groupItems, selection: $vm.selectedGroupId){
                 Text($0.group_name ?? "")
             }
         } content: {
             HStack(spacing: 0) {
                 Spacer(minLength: 0)
-                if let selectedGroup = selectedGroup {
-                    switch viewMode {
+                if let selectedGroup = vm.selectedGroup {
+                    switch vm.viewMode {
                     case .OVERVIEW:
                         OverviewModeView(pg: selectedGroup)
                     case .SCREENSHOTS:
@@ -60,18 +39,18 @@ struct ContentView: View {
                 }
                 Spacer(minLength: 0)
                 Divider()
-                if showDetail {
+                if vm.showDetail {
                     MasterNoteView()
                 }
             }
-            .navigationTitle(contentTitle)
+            .navigationTitle(vm.contentTitle)
         } detail: {
             Spacer()
                 .navigationSplitViewColumnWidth(0)
         }
         .toolbar {
             ToolbarItemGroup(placement: .navigation) {
-                Picker("Mode", selection: $viewMode) {
+                Picker("Mode", selection: $vm.viewMode) {
                     Text(ViewMode.OVERVIEW.rawValue).tag(ViewMode.OVERVIEW)
                     Text(ViewMode.SCREENSHOTS.rawValue).tag(ViewMode.SCREENSHOTS)
                     Text(ViewMode.ICONS.rawValue).tag(ViewMode.ICONS)
@@ -87,27 +66,37 @@ struct ContentView: View {
                 Spacer()
                 Button(action: {
                     withAnimation {
-                        showDetail.toggle()
+                        vm.showDetail.toggle()
                     }
                 }) {
                     Image(systemName: "sidebar.right")
                 }
             }
         }
-        .onChange(of: selectedGroup) {
-            if let selectedGroup = selectedGroup {
-                contentTitle = selectedGroup.group_name ?? ""
+        .onChange(of: vm.selectedGroup) {
+            if let selectedGroup = vm.selectedGroup {
+                vm.contentTitle = selectedGroup.group_name ?? ""
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+extension ContentView {
+    @Observable class ViewModel {
+        var pageGroupList: [PageGroup] = []
+        var showDetail = false
+        var selectedGroupId: PageGroup.ID? = nil
+        var selectedGroup: PageGroup? {
+            if let selectedGroupId = selectedGroupId {
+                return pageGroupList.first(where: {e in e.id == selectedGroupId})
+            }
+            
+            return nil
+        }
+        var contentTitle: String = "ASR"
+        var viewMode: ViewMode = .OVERVIEW
+    }
+}
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
